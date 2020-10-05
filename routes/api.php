cfffaa1c76e328836efdb355ae076e34a6f3a395
DaniloEV <danilox.espinoza@gmail.com>
2020-10-04 22:24:11 -0600

    comentario api.php

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,8 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+
+| Esto es un comentario para conseguir la play 5 :v aa
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
